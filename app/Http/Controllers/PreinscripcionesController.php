a9469d8be354d653bc695b82d652a279c1a49a27
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-10-10 14:31:20 -0500

    CRUD Preinscripciones

--- app/Http/Controllers/PreinscripcionesController.php
+++ app/Http/Controllers/PreinscripcionesController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Preinscripciones;
+use App\Http\Resources\Preinscripciones as PreinscripcionesResource;
+use Validator;
+use Illuminate\Http\Response;
 
 class PreinscripcionesController extends Controller
 {
@@ -13,17 +17,13 @@ class PreinscripcionesController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $preinscripciones = Preinscripciones::all();
+        if ($preinscripciones != null){
+            $conversion = PreinscripcionesResource::collection($preinscripciones);
+            return response()->json($conversion)->setStatusCode(200);
+        }
+        $message = array("message" => "No se encontraron elementos.");
+        return response()->json($message)->setStatusCode(404);
     }
 
     /**
@@ -34,7 +34,31 @@ class PreinscripcionesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'string' => 'El campo: :attribute, debe de ser texto.',
+            'exists' => 'El valor del campo: :attribute no existe en la tabla que hace referencia.',
+            'date' => 'El campo: :attribute, debe de ser una fecha con el formato adecuado.',
+            'integer' => 'El campo: :attribute, debe contener números enteros.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'mascota_id' => 'required|integer|exists:mascotas,id',
+            'medicamento_id' => 'required|integer|exists:medicamentos,id',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $preinscripcionCrear = new Preinscripciones;
+            $preinscripcionCrear->mascota_id = $request->input('mascota_id');
+            $preinscripcionCrear->medicamento_id = $request->input('medicamento_id');
+            $preinscripcionCrear->save();
+            return response()->json($preinscripcionCrear)->setStatusCode(201);
+        }
     }
 
     /**
@@ -45,20 +69,14 @@ class PreinscripcionesController extends Controller
      */
     public function show($id)
     {
-        //
+        $preinscripcionMostrar = Preinscripciones::findOrFail($id);
+        if ($preinscripcionMostrar != null){
+            $conversion = new PreinscripcionesResource($preinscripcionMostrar);
+            return response()->json($conversion)->setStatusCode(200);
+        }
+        $message = array("message" => "No se encontraro el elemento.");
+        return response()->json($message)->setStatusCode(404);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      *
@@ -68,7 +86,31 @@ class PreinscripcionesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'string' => 'El campo: :attribute, debe de ser texto.',
+            'exists' => 'El valor del campo: :attribute no existe en la tabla que hace referencia.',
+            'date' => 'El campo: :attribute, debe de ser una fecha con el formato adecuado.',
+            'integer' => 'El campo: :attribute, debe contener números enteros.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'mascota_id' => 'required|integer|exists:mascotas,id',
+            'medicamento_id' => 'required|integer|exists:medicamentos,id',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $preinscripcionActualizar = Preinscripciones::findOrFail($id);
+            $preinscripcionActualizar->mascota_id = $request->input('mascota_id');
+            $preinscripcionActualizar->medicamento_id = $request->input('medicamento_id');
+            $preinscripcionActualizar->save();
+            return response()->json($preinscripcionActualizar)->setStatusCode(201);
+        }
     }
 
     /**
@@ -79,6 +121,13 @@ class PreinscripcionesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $preinscripciones = Preinscripciones::where('id',$id)->first();
+        if ($preinscripciones != null){
+            $preinscripciones->delete();
+            $message = array("message" => "Elemento eliminado correctamente.");
+            return response()->json($message)->setStatusCode(200);
+        }
+        $message = array("message" => "El elemento ya ha sido eliminado.");
+        return response()->json($message)->setStatusCode(410);
     }
 }
--- app/Http/Resources/Preinscripciones.php
+++ app/Http/Resources/Preinscripciones.php
@@ -14,6 +14,12 @@ class Preinscripciones extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'mascota_id' => $this->mascota_id,
+            'medicamento_id' => $this->medicamento_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
