02d25bc79272858ce11ffea694af46357e0ce220
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-10-10 14:05:09 -0500

    CRUD Operaciones

--- app/Http/Controllers/OperacionesController.php
+++ app/Http/Controllers/OperacionesController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Operaciones;
+use App\Http\Resources\Operaciones as OperacionesResource;
+use Validator;
+use Illuminate\Http\Response;
 
 class OperacionesController extends Controller
 {
@@ -13,17 +17,13 @@ class OperacionesController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $operaciones = Operaciones::all();
+        if ($operaciones != null){
+            $conversion =OperacionesResource::collection($operaciones);
+            return response()->json($conversion)->setStatusCode(200);
+        }
+        $message = array("message" => "No se encontraron elementos.");
+        return response()->json($message)->setStatusCode(404);
     }
 
     /**
@@ -34,7 +34,37 @@ class OperacionesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'string' => 'El campo: :attribute, debe de ser texto.',
+            'exists' => 'El valor del campo: :attribute no existe en la tabla que hace referencia.',
+            'date' => 'El campo: :attribute, debe de ser una fecha con el formato adecuado.',
+            'integer' => 'El campo: :attribute, debe contener números enteros.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'mascota_id' => 'required|integer|exists:mascotas,id',
+            'doctor_id' => 'required|integer|exists:doctores,id',
+            'tipo' => 'required|string',
+            'sala' => 'required|string',
+            'fecha_hora' => 'required|date',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $operacionCrear = new Operaciones;
+            $operacionCrear->mascota_id = $request->input('mascota_id');
+            $operacionCrear->doctor_id = $request->input('doctor_id');
+            $operacionCrear->tipo = $request->input('tipo');
+            $operacionCrear->sala = $request->input('sala');
+            $operacionCrear->fecha_hora = $request->input('fecha_hora');
+            $operacionCrear->save();
+            return response()->json($operacionCrear)->setStatusCode(201);
+        }
     }
 
     /**
@@ -45,7 +75,13 @@ class OperacionesController extends Controller
      */
     public function show($id)
     {
-        //
+        $operacionesMostrar = Operaciones::findOrFail($id);
+        if ($operacionesMostrar != null){
+            $conversion = new OperacionesResource($operacionesMostrar);
+            return response()->json($conversion)->setStatusCode(200);
+        }
+        $message = array("message" => "No se encontraro el elemento.");
+        return response()->json($message)->setStatusCode(404);
     }
 
     /**
@@ -54,22 +90,41 @@ class OperacionesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'string' => 'El campo: :attribute, debe de ser texto.',
+            'exists' => 'El valor del campo: :attribute no existe en la tabla que hace referencia.',
+            'date' => 'El campo: :attribute, debe de ser una fecha con el formato adecuado.',
+            'integer' => 'El campo: :attribute, debe contener números enteros.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'mascota_id' => 'required|integer|exists:mascotas,id',
+            'doctor_id' => 'required|integer|exists:doctores,id',
+            'tipo' => 'required|string',
+            'sala' => 'required|string',
+            'fecha_hora' => 'required|date',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $operacionActualizar = Operaciones::findOrFail($id);
+            $operacionActualizar->mascota_id = $request->input('mascota_id');
+            $operacionActualizar->doctor_id = $request->input('doctor_id');
+            $operacionActualizar->tipo = $request->input('tipo');
+            $operacionActualizar->sala = $request->input('sala');
+            $operacionActualizar->fecha_hora = $request->input('fecha_hora');
+            $operacionActualizar->save();
+            return response()->json($operacionActualizar)->setStatusCode(201);
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
@@ -79,6 +134,13 @@ class OperacionesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $operaciones = Operaciones::where('id',$id)->first();
+        if ($operaciones != null){
+            $operaciones->delete();
+            $message = array("message" => "Elemento eliminado correctamente.");
+            return response()->json($message)->setStatusCode(200);
+        }
+        $message = array("message" => "El elemento ya ha sido eliminado.");
+        return response()->json($message)->setStatusCode(410);
     }
 }
--- app/Http/Resources/Operaciones.php
+++ app/Http/Resources/Operaciones.php
@@ -14,6 +14,13 @@ class Operaciones extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'mascota_id' => $this->mascota_id,
+            'doctor_id' => $this->doctor_id,
+            'tipo' => $this->tipo,
+            'sala' => $this->sala,
+            'fecha_hora' => $this->fecha_hora
+        ];
     }
 }
